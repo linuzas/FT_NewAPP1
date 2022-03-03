74c3b69ca9b8cce3e29ba9977ae331c962c66eb2
linuzas <100625146+linuzas@users.noreply.github.com>
2022-03-03 22:50:41 +0200

    Revert "Revert "Button for color change added""
    This reverts commit 7b98d06de072559ad98ff759c8796c13be4530a2.

--- app/src/main/java/com/example/ft_newapp/MainActivity.java
+++ app/src/main/java/com/example/ft_newapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.ft_newapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       this.tvMain = (TextView)findViewById(R.id.tvMain);
+       this.tvMain.setText("Push button to change text");
+    }
+
+
+
+    public void onBtnMainClck(View view) {
+
+        this.tvMain.setText("Text was changed");
+    }
+
+    public void onBtnTxtClrClick(View view) {
+        tvMain.setTextColor(Color.GREEN);
+
+
     }
 }
